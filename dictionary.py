d31e4f019e17abeb69dad827013850d008bf5c08
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-03-09 19:49:18 +0100

    plots added

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,10 @@
 import re
+import matplotlib.pyplot as plt
 
 # Räkna ut de 30 vanligaste orden i Romeo och Julia
 # med en dictionary
 
-with open("pg1523.txt", "r", encoding="utf-8") as f:
+with open("romeo_and_juliet.txt", "r", encoding="utf-8") as f:
     text = f.read().lower()
 
 words = re.findall(r"\b\w+\b", text)
@@ -17,4 +18,28 @@ for word in words:
         occurrences[word] += 1
 
 res = sorted(occurrences.items(), key=lambda item: item[1], reverse=True)[:30]
-print(res)
+
+labels = []
+values = []
+
+for r in res:
+    labels.append(r[0].title())
+    values.append(r[1])
+
+
+plt.bar(labels, values)
+plt.title("Most common words in Romeo and Juliet")
+plt.xlabel("Word")
+plt.ylabel("Count")
+plt.xticks(rotation=90)
+plt.show()
+
+
+values = [(l/len(words)*100) for l in values]
+
+plt.bar(labels, values)
+plt.title("Most common words in Romeo and Juliet")
+plt.xlabel("Word")
+plt.ylabel("Percentage (%) of all words")
+plt.xticks(rotation=90)
+plt.show()
